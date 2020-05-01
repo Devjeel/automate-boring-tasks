cd841a95a626b8cf7b3417ec93f732e4f9a5e449
devjeel <jeelhp02@gmail.com>
2020-04-30 22:58:58 -0400

    Py functions added

--- .cli_commands.sh
+++ .cli_commands.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 function shutdown() {
-    python script.py
+    python -c 'import script; script.shutdown()'
+}
+
+function restart(){
+    python -c 'import script; script.restart()'
+}
+
+function logout(){
+    python -c 'import script; script.logout()'
 }
