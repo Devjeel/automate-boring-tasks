fd0f5edde39b1a3d3a17c521a61b811bc50b8f73
devjeel <jeelhp02@gmail.com>
2020-05-04 17:32:55 -0400

    Update env variables

--- github_custom.py
+++ github_custom.py
@@ -1,7 +1,13 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from github import Github
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 # using an access token
-g = Github("<<token>>")
+g = Github(os.environ.get("GITHUB_API"))
 
 def get_all_repos:
     for repo in g.get_user().get_repos():
